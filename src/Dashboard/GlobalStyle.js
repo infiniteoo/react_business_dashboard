7b63be101361e472a451d5cbb75ff98623a45a56
Troy Dorman <troydorman@gmail.com>
2021-03-18 09:55:06 -0600

    add globalstyles

---
+++ src/Dashboard/GlobalStyle.js
@@ -0,0 +1,16 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const darkGrey = "#515263";
+const lightGrey = "#f7f7f7";
+
+
+export const GlobalStyle = createGlobalStyle`
+
+body {
+    color: ${darkGrey};
+    background-color: ${lightGrey};
+    font-family: 'Blinker', sans-serif;
+}
+
+`
+
