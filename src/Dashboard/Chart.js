9fcd3b67909a520d8652da71b11f2bc3c16ff5e7
Troy Dorman <troydorman@gmail.com>
2021-03-19 09:21:45 -0600

    build proper readme with animated gif and live site demo link

--- src/Dashboard/Chart.js
+++ src/Dashboard/Chart.js
@@ -1,92 +1,97 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Card } from "./Card";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-import {chartData, tableRows} from './Data'
-import { ThemeContext } from './Dashboard';
-import { darkerGrey, lightGrey } from './GlobalStyle';
+import { chartData } from "./Data";
+import { ThemeContext } from "./Dashboard";
+import { darkerGrey, lightGrey } from "./GlobalStyle";
 
-function getOptions(dark){
-return{
-  chart: {
-    height: "360px",
-    type: "column",
-    backgroundColor: dark ? darkerGrey : 'white',
-    style: {
-      fontFamily: `'Blinker', sans-serif`,
-    },
-  },
-  title: {
-    text: "Revenue by Product",
-    style: {
-      color: dark ? lightGrey : 'black'
-    }
-  },
-  xAxis: {
-    labels: {
+function getOptions(dark) {
+  return {
+    chart: {
+      height: "360px",
+      type: "column",
+      backgroundColor: dark ? darkerGrey : "white",
       style: {
-        color: dark ? lightGrey : 'black'
-      }
+        fontFamily: `'Blinker', sans-serif`,
+      },
     },
-    categories: ["Jan", "Feb", "Mar", 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-  },
-  yAxis: {
-    gridLineColor: dark ? 'grey' : lightGrey,
-    labels: {
-      style: {
-        color: dark ? lightGrey : 'black'
-      }
-    },
-    min: 0,
-    reversedStacks: false,
     title: {
+      text: "Revenue by Product",
       style: {
-        color: dark ? lightGrey : 'black',
-        fontSize: '14px'
+        color: dark ? lightGrey : "black",
       },
-      text: "Billions of Dollars",
     },
-  },
-  tooptip: {
-    pointFormat:
-      '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage.0f}%)<br/>',
-    shared: true,
-    style: {
-      backgroundColor: dark ? darkerGrey : 'white',
-      color: dark ? lightGrey : 'black'
-    }
-  },
-  plotOptions: {
+    xAxis: {
+      labels: {
+        style: {
+          color: dark ? lightGrey : "black",
+        },
+      },
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+    },
+    yAxis: {
+      gridLineColor: dark ? "grey" : lightGrey,
+      labels: {
+        style: {
+          color: dark ? lightGrey : "black",
+        },
+      },
+      min: 0,
+      reversedStacks: false,
+      title: {
+        style: {
+          color: dark ? lightGrey : "black",
+          fontSize: "14px",
+        },
+        text: "Billions of Dollars",
+      },
+    },
+    tooptip: {
+      pointFormat:
+        '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.percentage.0f}%)<br/>',
+      shared: true,
+      style: {
+        backgroundColor: dark ? darkerGrey : "white",
+        color: dark ? lightGrey : "black",
+      },
+    },
+    plotOptions: {
       series: {
-          borderWidth: 0
+        borderWidth: 0,
       },
-    column: {
-      stacking: "normal",
+      column: {
+        stacking: "normal",
+      },
     },
-  },
-  legend: {
-    itemStyle: {
-      color: dark ? lightGrey : 'black'
-    }
-  },
-  series: chartData
-
-
+    legend: {
+      itemStyle: {
+        color: dark ? lightGrey : "black",
+      },
+    },
+    series: chartData,
+  };
 }
-
-
-}
-
 
 export function Chart() {
   const [theme, setTheme] = useContext(ThemeContext);
-  const dark = theme === 'dark';
+  const dark = theme === "dark";
   return (
     <Card height={400} dark={dark}>
       <HighchartsReact highcharts={Highcharts} options={getOptions(dark)} />
     </Card>
   );
 }
-
-
-
--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -2,7 +2,6 @@ import React, { useState, createContext } from "react";
 import { Brand } from "./Brand";
 import { Container } from "./Container";
 import { GlobalStyle } from "./GlobalStyle";
-import { Card } from './Card' 
 import { Chart } from './Chart'
 import {VirtualizedTable} from './Table'
 import { DarkSwitch } from './DarkSwitch';
